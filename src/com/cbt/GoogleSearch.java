37ae40f4bde53a67d618b08078eb49fe975171e2
muhtar32 <maitusong2017@gmail.com>
2018-06-09 17:21:49 -0400

    add Etsy.com

---
+++ src/com/cbt/GoogleSearch.java
@@ -0,0 +1,17 @@
+package com.cbt;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleSearch {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver",
+				"/Users/muhtar/Documents/selenium dependencies/drivers/chromedriver");
+		
+		WebDriver driver=new ChromeDriver();
+		driver.get("http://www.etsy.com");
+
+	}
+
+}
